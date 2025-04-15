e0cb7ea087ce6df671d1e0223502576057219f93
Arnob Chanda <arnob.chanda@aesi-ess.com>
2025-04-16 00:42:48 +0530

    Updating sylua format

--- lua/plugins/toggletasks.lua
+++ lua/plugins/toggletasks.lua
@@ -1,22 +1,22 @@
 return {
-  "jedrzejboczar/toggletasks.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "akinsho/toggleterm.nvim",
-    "nvim-telescope/telescope.nvim",
-  },
-  rocks = "lyaml", -- To enable YAML config support
-  opts = {
-    debug = false,
-    silent = false,
-    short_paths = true,
-  },
-  keys = {
-    { "<leader>ts", "<cmd>Telescope toggletasks spawn<CR>", desc = "Toggletasks Spawn" },
-    { "<leader>tc", "<cmd>Telescope toggletasks select<CR>", desc = "Toggletasks Select" },
-    { "<leader>te", "<cmd>Telescope toggletasks edit<CR>", desc = "Toggletasks edit" },
-  },
-  init = function()
-    require("telescope").load_extension("toggletasks")
-  end,
+    "jedrzejboczar/toggletasks.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "akinsho/toggleterm.nvim",
+        "nvim-telescope/telescope.nvim",
+    },
+    rocks = "lyaml", -- To enable YAML config support
+    opts = {
+        debug = false,
+        silent = false,
+        short_paths = true,
+    },
+    keys = {
+        { "<leader>ts", "<cmd>Telescope toggletasks spawn<CR>", desc = "Toggletasks Spawn" },
+        { "<leader>tc", "<cmd>Telescope toggletasks select<CR>", desc = "Toggletasks Select" },
+        { "<leader>te", "<cmd>Telescope toggletasks edit<CR>", desc = "Toggletasks edit" },
+    },
+    init = function()
+        require("telescope").load_extension("toggletasks")
+    end,
 }
