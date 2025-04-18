6f24f2df7db3cad72fbfdae14da63b226bc5baa2
Arnob Chanda <arnob.chanda@aesi-ess.com>
2025-04-18 13:07:40 +0530

    New diag. Added a python venv selector

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,32 @@ local map = vim.keymap.set
 map("n", "<F7>", '<cmd>execute v:count . "ToggleTerm"<CR>', { desc = "Toggle Terminal" })
 map("t", "<F7>", "<cmd>ToggleTerm<CR>", { desc = "Toggle Terminal" })
 
+vim.keymap.set("n", "<leader>gR", function()
+    local file = vim.fn.expand("%:p")
+    if file == "" then
+        print("No file open!")
+        return
+    end
+
+    local file_name = vim.fn.expand("%:t")
+    local cmd = "git checkout HEAD -- " .. file
+    print("Running: " .. cmd)
+
+    local result = vim.fn.system(cmd)
+    if vim.v.shell_error ~= 0 then
+        print("Git error: " .. result)
+    else
+        vim.cmd("edit!")
+        print("Reset file " .. file_name .. " to HEAD")
+    end
+end, { desc = "Reset current file to HEAD" })
+
+-- Move line up
+map("n", "<A-Up>", ":m .-2<CR>==", { noremap = true, silent = true })
+map("i", "<A-Up>", "<Esc>:m .-2<CR>==gi", { noremap = true, silent = true })
+map("v", "<A-Up>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+
+-- Move line down
+map("n", "<A-Down>", ":m .+1<CR>==", { noremap = true, silent = true })
+map("i", "<A-Down>", "<Esc>:m .+1<CR>==gi", { noremap = true, silent = true })
+map("v", "<A-Down>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
