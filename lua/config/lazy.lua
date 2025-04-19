2aa97a1dc1bfe0ee6e73bf807ae791b8b8c16e20
Arnob Chanda <arnob.chanda@aesi-ess.com>
2025-04-19 18:01:35 +0530

    Some more extra plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,7 +19,15 @@ require("lazy").setup({
         -- add LazyVim and import its plugins
         { "LazyVim/LazyVim", import = "lazyvim.plugins" },
         -- import/override with your plugins
+        { import = "lazyvim.plugins.extras.dap.core" },
         { import = "lazyvim.plugins.extras.coding.mini-comment" },
+        { import = "lazyvim.plugins.extras.coding.nvim-cmp" },
+        { import = "lazyvim.plugins.extras.formatting.prettier" },
+        { import = "lazyvim.plugins.extras.editor.outline" },
+        { import = "lazyvim.plugins.extras.editor.harpoon2" },
+        { import = "lazyvim.plugins.extras.lang.clangd" },
+        { import = "lazyvim.plugins.extras.lang.python" },
+        { import = "lazyvim.plugins.extras.ui.treesitter-context" },
         { import = "plugins" },
     },
     defaults = {
