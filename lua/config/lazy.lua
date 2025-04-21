a1fb38fdacd1b347cf2e012e89d89361e6ee804c
Arnob Chanda <arnob.chanda@aesi-ess.com>
2025-04-21 17:40:30 +0530

    Adding mini-surround

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,6 +21,7 @@ require("lazy").setup({
         { import = "lazyvim.plugins.extras.dap.core" },
         { import = "lazyvim.plugins.extras.coding.mini-comment" },
         { import = "lazyvim.plugins.extras.coding.nvim-cmp" },
+        { import = "lazyvim.plugins.extras.coding.mini-surround" },
         { import = "lazyvim.plugins.extras.formatting.prettier" },
         { import = "lazyvim.plugins.extras.editor.outline" },
         { import = "lazyvim.plugins.extras.editor.harpoon2" },
